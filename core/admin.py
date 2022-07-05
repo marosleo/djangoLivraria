59a6f4e18ea0aaaae26632d6b7b0395be60fcb14
coffeau <eduarda6saibert@gmail.com>
2022-07-05 15:14:05 -0300

    newClass Livro

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from core.models import Categoria, Editora, Nome
+from core.models import Categoria, Editora, Autor, Livro
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
-admin.site.register(Nome)
+admin.site.register(Autor)
+admin.site.register(Livro)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
@@ -7,15 +8,23 @@ class Categoria(models.Model):
     def __str__(self):
         return self.descricao
 
-
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(null=True, blank=True)
-
     def __str__(self):
         return self.nome
 
-class Nome(models.Model):
-    nome = models.CharField(max_length=100)
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
     def __str__(self):
         return self.nome
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
